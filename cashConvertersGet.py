b270696f0b8a911c8759f36c827ce3cbcd415e22
Mep609 <hal@kolb.co.uk>
2019-12-29 16:01:28 +0000

    Add catagory search.

--- cashConvertersGet.py
+++ cashConvertersGet.py
@@ -19,7 +19,7 @@ numPages = int(numResults)/9 + 1 #Claculates number of pages at 9 items per page
 
 products = [] #Initialises products list
 
-#numPages = 1 #Temporarily overwrite number of pages to one to save time when debugging
+numPages = 3 #Temporarily overwrite number of pages to one to save time when debugging
 
 for i in range(numPages): #Loop trough all pages of restuls
     res = requests.get('https://www.cashconverters.co.uk/' + shopName + '/products?page=' + str(i))
@@ -43,5 +43,12 @@ Name == "data-name"
 Price == "data-price"
 href == "href"
 '''
+
+for i in range(len(products)):
+    print(products[i]["data-category"])
+    if "Musical Instruments/ Guitars" in products[i]["data-category"]:
+        print("found")
+'''
 for i in range(len(products)):
     print("{0:70.70}:   {1:5}".format(products[i]["data-name"].title().encode("utf-8"), products[i]["data-price"].title().encode("utf-8")))
+'''
