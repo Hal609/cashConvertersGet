e44bdd4c15e31b5f99c306182e21b4617c327488
Mep609 <hal@kolb.co.uk>
2019-12-29 14:48:26 +0000

    Comment code.

--- cashConvertersGet.py
+++ cashConvertersGet.py
@@ -1,32 +1,36 @@
 import requests, bs4
 
+#init variables
 shopName = "Blackpool-Church-Street"
 page = "1"
 
+#get first page of products for a location
 res = requests.get('https://www.cashconverters.co.uk/' + shopName + '/products?page=' + page)
 res.raise_for_status()
 
+#Load page HTML to BeautifulSoup variable
 ccSoup = bs4.BeautifulSoup(res.text, features="lxml")
 
-numResults = ccSoup.select('p em')[1].getText()
-numPages = int(numResults)/9 + 1
+#Find the number of restuls and thus the number of pages to check
+numResults = ccSoup.select('p em')[1].getText() #Grabs second <em> element within a <p> element
+numPages = int(numResults)/9 + 1 #Claculates number of pages at 9 items per page
 
 #divElems = ccSoup.select('div')
 
-products = []
+products = [] #Initialises products list
 
-numPages = 1
+numPages = 1 #Temporarily overwrite number of pages to one to save time when debugging
 
-for i in range(numPages):
+for i in range(numPages): #Loop trough all pages of restuls
     res = requests.get('https://www.cashconverters.co.uk/' + shopName + '/products?page=' + str(i))
     res.raise_for_status()
 
     ccSoup = bs4.BeautifulSoup(res.text, features="lxml")
 
-    divElems = ccSoup.select('div')
+    divElems = ccSoup.select('div') #Selct <div> elements
 
-    for j in range(len(divElems)):
+    for j in range(len(divElems)): #Check each <div> elem to see if it is a product
         if divElems[j].get("class") == ['panel', 'panel-default', 'product-panel']:
             products.append(divElems[j])
 
-print(products[1]["data-name"])
+print(products[2]["data-name"])
