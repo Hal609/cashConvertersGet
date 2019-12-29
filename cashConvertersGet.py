a249279e6a756bc551fa83c71d0bb1f889c99edb
Mep609 <hal@kolb.co.uk>
2019-12-29 21:05:53 +0000

    Clean up code.

--- cashConvertersGet.py
+++ cashConvertersGet.py
@@ -1,37 +1,55 @@
 import requests, bs4
 
-#init variables
-shopName = "Blackpool-Church-Street"
-page = "1"
-
-#get first page of products for a location
-res = requests.get('https://www.cashconverters.co.uk/' + shopName + '/products?page=' + page)
-res.raise_for_status()
-
-#Load page HTML to BeautifulSoup variable
-ccSoup = bs4.BeautifulSoup(res.text, features="lxml")
-
-#Find the number of restuls and thus the number of pages to check
-numResults = ccSoup.select('p em')[1].getText() #Grabs second <em> element within a <p> element
-numPages = int(numResults)/9 + 1 #Claculates number of pages at 9 items per page
-
-#divElems = ccSoup.select('div')
-
-products = [] #Initialises products list
-
-numPages = 5 #Temporarily overwrite number of pages to one to save time when debugging
-
-for i in range(numPages): #Loop trough all pages of restuls
-    res = requests.get('https://www.cashconverters.co.uk/' + shopName + '/products?page=' + str(i))
+def loadPage(shopName, page):
+    #Get page of products for a location
+    res = requests.get('https://www.cashconverters.co.uk/' + shopName + '/products?page=' + str(page))
     res.raise_for_status()
 
-    ccSoup = bs4.BeautifulSoup(res.text, features="lxml")
+    return(bs4.BeautifulSoup(res.text, features="lxml")) #Return page HTML as BeautifulSoup variable
 
-    divElems = ccSoup.select('div') #Selct <div> elements
+    #return(ccSoup.select('div')) #Selct <div> elements
 
-    for j in range(len(divElems)): #Check each <div> elem to see if it is a product
-        if divElems[j].get("class") == ['panel', 'panel-default', 'product-panel']:
-            products.append(divElems[j])
+def pageCount(shopName):
+    ccSoup = loadPage(shopName, 1)
+
+    #Find the number of restuls and thus the number of pages to check
+    numResults = ccSoup.select('p em')[1].getText() #Grabs second <em> element within a <p> element
+    return(int(numResults)/9 + 1) #Claculates number of pages at 9 items per page
+
+def loadProducts(shopName):
+    numPages = pageCount(shopName)
+    numPages = 5
+
+    for i in range(numPages): #Loop trough all pages
+        ccSoup = loadPage(shopName, i) #Load each page
+        divElems = ccSoup.select('div') #Select div elements
+
+        for j in range(len(divElems)): #Check each <div> elem to see if it is a product
+            if divElems[j].get("class") == ['panel', 'panel-default', 'product-panel']:
+                products.append(divElems[j])
+
+def productSearch(searchTerm, *secondTerm):
+    for i in range(len(products)):
+        cat = products[i]["data-category"].lower()
+        name = products[i]["data-name"].lower()
+        if searchTerm in cat or searchTerm in name:
+            print("{0:70.70}:   {1:5}".format(products[i]["data-name"].title().encode("utf-8"), products[i]["data-price"].title().encode("utf-8")))
+            print(products[i].find_all("a")[0].get("href"))
+
+
+#init variables
+#shopName = "Blackpool-Church-Street"
+page = "1"
+products = []
+
+#numPages = pageCount(shopName)
+#numPages = 5 #Temporarily overwrite number of pages to one to save time when debugging
+
+loadProducts("Blackpool-Church-Street")
+
+productSearch("guitar")
+productSearch("tv")
+
 
 '''
 Product Info
@@ -44,12 +62,6 @@ Price == "data-price"
 href == "href"
 '''
 
-searchTerm = "guitar"
-
-for i in range(len(products)):
-    if searchTerm in products[i]["data-category"].lower() or searchTerm in products[i]["data-name"].lower():
-        print("{0:70.70}:   {1:5}".format(products[i]["data-name"].title().encode("utf-8"), products[i]["data-price"].title().encode("utf-8")))
-        print(products[i].find_all("a")[0].get("href"))
 '''
 for i in range(len(products)):
     print("{0:70.70}:   {1:5}".format(products[i]["data-name"].title().encode("utf-8"), products[i]["data-price"].title().encode("utf-8")))
