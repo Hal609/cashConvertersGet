bd5e1c47be9287d75b1c38299aa5c3c7b1bdec1f
Mep609 <hal@kolb.co.uk>
2019-12-30 15:33:38 +0000

    Sort results by price and time functions.

--- cashConvertersGet.py
+++ cashConvertersGet.py
@@ -1,4 +1,4 @@
-import requests, bs4
+import requests, bs4, time
 
 def loadPage(shopName, page):
     #Get page of products for a location
@@ -7,7 +7,6 @@ def loadPage(shopName, page):
 
     return(bs4.BeautifulSoup(res.text, features="lxml")) #Return page HTML as BeautifulSoup variable
 
-    #return(ccSoup.select('div')) #Selct <div> elements
 
 def pageCount(shopName):
     ccSoup = loadPage(shopName, 1)
@@ -17,12 +16,13 @@ def pageCount(shopName):
     return(int(int(numResults)/9 + 1)) #Claculates number of pages at 9 items per page
 
 def loadProducts(shopName):
-    products.clear()
+    products.clear() #Removes previous shops products from list
 
-    with open(outputFile, "a") as out:
+    with open(outputFile, "a") as out: #Write shop name as tiltle
         out.write("\n\n" + shopName + "\n" + "="*len(shopName) + "\n")
 
-    numPages = pageCount(shopName)
+    numPages = pageCount(shopName) #Find the number of pages of products
+    numPages = 5
 
     for i in range(numPages): #Loop trough all pages
         ccSoup = loadPage(shopName, i) #Load each page
@@ -32,9 +32,10 @@ def loadProducts(shopName):
             if divElems[j].get("class") == ['panel', 'panel-default', 'product-panel']:
                 products.append(divElems[j])
 
-
 def productSearch(*searchTerms):
     found = []
+    entry = ()
+    results = []
 
     with open(outputFile, "a") as out:
         out.write("{0:60.60}    {1:6}     {2:100} \n\n".format("Item", "Price", "URL"))
@@ -43,17 +44,28 @@ def productSearch(*searchTerms):
         cat = products[id]["data-category"].lower()
         name = products[id]["data-name"].lower()
 
+
         for search in searchTerms:
 
             if search[0] == "-": #If negative search term
-                search = search[1:]
-                if search in cat or search in name:
+                search = search[1:] #Remove negative
+                if search in cat or search in name: #search for term
                     if id in found:
-                        found.remove(id)
+                        found.remove(id) #Remove matching terms from results
             elif search in cat or search in name:
                 if id not in found:
-                    found.append(id)
+                    found.append(id) #Add matching terms to results
+
     for id in found:
+        entry = (float(products[id]["data-price"]), id)
+        results.append(entry)
+
+    print(results)
+    results.sort()
+    print(results)
+
+    for prod in results:
+        id = prod[1]
         result = "{0:60.60}:   {1:>6.6}     {2:100}".format(products[id]["data-name"].title(), products[id]["data-price"].title(), products[id].find_all("a")[0].get("href"))
         with open(outputFile, "a") as out:
             out.write(result + "\n")
@@ -62,28 +74,16 @@ products = []
 outputFile = "/home/hal/Documents/PythonScripts/cashConverters/Output.txt"
 
 with open(outputFile, "w") as out:
-    #out.write("CASH CONVERTERS SEARCH TOOL\n---------------------------")
     out.write("╔═╗┌─┐┌─┐┬ ┬╔═╗┌─┐┬  ┬┌─┐┬─┐┌┬┐┌─┐┬─┐┌─┐╔╦╗┌─┐┌─┐┬\n║  ├─┤└─┐├─┤║  │ │└┐┌┘├┤ ├┬┘ │ ├┤ ├┬┘└─┐ ║ │ ││ ││\n╚═╝┴ ┴└─┘┴ ┴╚═╝└─┘ └┘ └─┘┴└─ ┴ └─┘┴└─└─┘ ╩ └─┘└─┘┴─┘\n")
 
-shops = ["Sheffield-Gleadless-Road", "Sheffield-Hillsborough", "Preston", "Blackpool-Church-Street", "Blackpool-South-Shore"]
-#shops = ["Blackpool-Church-Street"]
+#shops = ["Sheffield-Gleadless-Road", "Sheffield-Hillsborough", "Preston", "Blackpool-Church-Street", "Blackpool-South-Shore"]
+shops = ["Blackpool-Church-Street"]
 
 for shop in shops:
+    initTime = time.time()
     loadProducts(shop)
-    productSearch("guitar", "-amp", "-amplifier", "-tuner")
-
-'''
-Product Info
-============
-Barcode == "data-barcode"
-Brand == "data-brand"
-Category == "data-category"
-Name == "data-name"
-Price == "data-price"
-href == "href"
-'''
-
-'''
-for i in range(len(products)):
-    print("{0:70.70}:   {1:5}".format(products[i]["data-name"].title().encode("utf-8"), products[i]["data-price"].title().encode("utf-8")))
-'''
+    print("Prod load time:", (time.time() - initTime))
+    initTime = time.time()
+    #productSearch("guitar", "instrument", "-junior", "-kid", "-child", "-1/2", "-3/4", "-amp", "-amplifier", "-tuner", "-pedal")
+    productSearch("ring")
+    print("Search time:", (time.time() - initTime))
